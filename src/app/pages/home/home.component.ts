3affd32e620d065aab067a03949ce391be592b52
Dario Coletto <darocoletto@gmail.com>
2025-05-29 02:09:09 -0300

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,5 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+    indexSelected = 0;
+
+    switchText(index: number){
+        this.toggleClass(index);
+        this.indexSelected = index;
+       
+    }
+
+    //A funtion that toggles the class active and let the other elemetnts without the class active
+    // It receives the index of the element to toggle
+    toggleClass(index: number) {
+        const elements = document.querySelectorAll('.title');
+        elements.forEach((element, i) => {
+            if (i === index) {
+                element.classList.add('active');
+            } else {
+                element.classList.remove('active');
+            }
+        });
+
+    }
+
    
 }

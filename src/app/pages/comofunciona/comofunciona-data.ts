747c014caa70138b4fce5ffd9055b66fa4342b0c
Dario Coletto <darocoletto@gmail.com>
2025-06-19 02:01:26 -0300

    update text content

--- src/app/pages/comofunciona/comofunciona-data.ts
+++ src/app/pages/comofunciona/comofunciona-data.ts
@@ -6,34 +6,27 @@ export type CardInfo = {
 
 export const cardInfo: CardInfo = [
   {
-    title: 'Sistema de Recolección',
-    content: `La geotermia para climatización utiliza perforaciones en el terreno
-          para instalar tuberías a cierta profundidad. Estas tuberías, llamadas
-          sondas geotérmicas, circulan un fluido que recoge el calor del suelo
-          en invierno y lo transfiere al edificio. En verano, el proceso se
-          invierte: el calor del edificio es expulsado al subsuelo. Las
-          perforaciones suelen tener entre 50 y 200 metros de profundidad,
-          dependiendo del sistema y las características del terreno.`,
-    img: '/garden-400.jpg',
-  },
-  {
-    title: 'Sistema de Distribución',
-    content: `El calor recogido se distribuye en el edificio mediante sistemas de
-          suelo radiante o fan coils. El suelo radiante proporciona tanto
-          calefacción como refrigeración, distribuyendo el calor o el frío de
-          manera uniforme y confortable desde el piso. Los fan coils, por su
-          parte, distribuyen el aire caliente o frío de manera eficiente por
-          todo el espacio. Ambos sistemas garantizan una climatización eficiente
-          y sostenible.`,
+    title: 'Sistema de captación',
+    content: `La geotermia para climatización utiliza sondas que viajan por el subsuelo a cierta profundidad. 
+    Estas sondas, capturan el calor del suelo en invierno y lo transfieren al edificio, mientras que en verano, el proceso se invierte para lograr el refrescamiento. 
+    Hay varias formas de captar la temperatura deseada por medio de las sondas, siendo nuestro geólogo el encargado de estudiar la zona para determinar el adecuado.
+`,
     img: '/garden-400.jpg',
   },
   {
     title: 'Bomba de Calor',
-    content: `Las bombas de calor geotérmicas son el corazón del sistema. Estas
-          bombas intercambian calor con el subsuelo, aprovechando la temperatura
-          constante del terreno durante todo el año. En invierno, extraen el
-          calor del suelo para calentar el edificio. En verano, expulsan el
-          calor del edificio hacia el subsuelo para enfriar.`,
+    content: `Las bombas de calor geotérmicas son el corazón del sistema. 
+    Estas bombas intercambian calor con el subsuelo, aprovechando la temperatura de 15°C promedio del terreno durante todo el año. 
+    En invierno, extrae el calor del suelo para calentar el edificio. En verano, expulsa el calor del edificio hacia el subsuelo para enfriar.`,
+    img: '/garden-400.jpg',
+  },
+  {
+    title: 'Sistema de Distribución',
+    content: `El frío/calor recogido se distribuye en el edificio mediante sistemas de suelo radiante y/o fan coils. 
+    El suelo radiante proporciona tanto calefacción como refrigeración, distribuyendo el calor o el frío de manera uniforme y confortable desde el piso. 
+    Los fan coils, por su parte, distribuyen el aire caliente o frío de manera eficiente por todo el espacio.
+     Ambos sistemas garantizan una climatización eficiente y sostenible.
+`,
     img: '/garden-400.jpg',
   },
 ];

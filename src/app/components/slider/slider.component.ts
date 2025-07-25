10fbb4d2e27807e01eb32909b3441aae15e1b958
Dario Coletto <darocoletto@gmail.com>
2025-07-25 17:12:37 -0300

    update

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,6 +1,7 @@
-import { Component,  signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {
   animate,
+  AnimationEvent,
   state,
   style,
   transition,
@@ -9,118 +10,188 @@ import {
 import { ProgressbarComponent } from '../progressbar/progressbar.component';
 import { IconComponent } from '../icon/icons.component';
 
- interface Slide {
-    title: string[];  
-    body: string[];
-    img: string;
-  }
 
+interface Slide {
+  title: string[];
+  body: string[];
+  img: string;
+}
 
 @Component({
-  standalone:true,
-  imports:[ProgressbarComponent, IconComponent],
-    selector: 'app-slider',
-    templateUrl: './slider.component.html',
-    styleUrls: ['./slider.component.css'],
-    animations: [
-        trigger('slideAnimation', [
-            // Estado por defecto
-            //state('default', style({ transform: 'translateX(0)' })),
-            // Transición hacia la izquierda
-            transition('void => toLeft', [
-                style({
-                    transform: 'translateX(-100%)',
-                    filter: 'blur(40px)',
-                    opacity: 0,
-                }),
-                animate('1s 0.2s cubic-bezier(0.230, 1.000, 0.320, 1.000)', style({
-                    transform: 'translateX(0)',
-                    filter: 'none',
-                    opacity: 1,
-                })),
-            ]),
-            // Transición hacia la derecha
-            transition('void => toRight', [
-                style({
-                    transform: 'translateX(100%)', // Confirmar que el punto inicial es 100%
-                    filter: 'blur(40px)',
-                    opacity: 0,
-                }),
-                animate('1s 0.2s cubic-bezier(0.230, 1.000, 0.320, 1.000)', style({
-                    transform: 'translateX(0)',
-                    filter: 'none',
-                    opacity: 1,
-                })),
-            ]),
-            // Salida hacia la izquierda
-            transition('toLeft => void', [
-                animate('1s cubic-bezier(0.230, 1.000, 0.320, 1.000)', style({
-                    transform: 'translateX(-100%)',
-                    filter: 'blur(40px)',
-                    opacity: 0,
-                })),
-            ]),
-            // Salida hacia la derecha
-            transition('toRight => void', [
-                animate('1s cubic-bezier(0.230, 1.000, 0.320, 1.000)', style({
-                    transform: 'translateX(100%)', // Confirmar que se mueve hacia la derecha
-                    filter: 'blur(40px)',
-                    opacity: 0,
-                })),
-            ]),
-        ]),
-    ]
+  standalone: true,
+  imports: [ProgressbarComponent, IconComponent],
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css'],
+  animations: [
+    trigger('slideAnimation', [
+      // Estado por defecto
+      state(
+        'default',
+        style({
+          transform: 'translateX(0)',
+          filter: 'none',
+          opacity: 1,
+          zIndex: 1,
+        })
+      ),
+
+      // Transición bidireccional sin cambios
+      transition('* <=> default', animate('0s')),
+
+      // Transición hacia la izquierda
+      transition('* => toLeft', [
+        style({
+          transform: 'translateX(100%)',
+          filter: 'blur(40px)',
+          opacity: 0,
+          zIndex: 2,
+        }),
+        animate(
+          '1.5s cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+          style({
+            transform: 'translateX(0)',
+            filter: 'none',
+            opacity: 1,
+            zIndex: 1,
+          })
+        ),
+      ]),
+
+      // Transición hacia la derecha
+      transition('* => toRight', [
+        style({
+          transform: 'translateX(-100%)',
+          filter: 'blur(40px)',
+          opacity: 0,
+          zIndex: 2,
+        }),
+        animate(
+          '1.5s cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+          style({
+            transform: 'translateX(0)',
+            filter: 'none',
+            opacity: 1,
+            zIndex: 1,
+          })
+        ),
+      ]),
+
+      // Salida hacia izquierda (cuando cambia a otro estado)
+      transition('toLeft => *', [
+        animate(
+          '1.5s cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+          style({
+            transform: 'translateX(-100%)',
+            filter: 'blur(40px)',
+            opacity: 0,
+            zIndex: 0,
+          })
+        ),
+      ]),
+
+      // Salida hacia derecha (cuando cambia a otro estado)
+      transition('toRight => *', [
+        animate(
+          '1.5s cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+          style({
+            transform: 'translateX(100%)',
+            filter: 'blur(40px)',
+            opacity: 0,
+            zIndex: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class SliderComponent {
-
-  
-  index = signal<number>(0)
-  slideIndex = 0;
-  fullBar:any;
-  direction: 'toLeft' | 'toRight'  = 'toRight';
-  auxActive:boolean = false;
- 
- 
+  index = 0;
+  indexA = 0;
+  indexB = 0;
+  fullBar: any;
+  direction: 'toLeft' | 'toRight' | 'default' = 'default';
+  auxActive: boolean = false;
 
   SLIDES = [
     {
       title: ['calefaccion'],
-      body: ['Mediante Losa Radiante', 'Ambientes unificados o diferenciados','Temperatura máxima recomendada en calor radiante: 21°C'],
-      img:'/garden-800.jpg'
+      body: [
+        'Mediante Losa Radiante',
+        'Ambientes unificados o diferenciados',
+        'Temperatura máxima recomendada en calor radiante: 21°C',
+      ],
+      img: '/paisajes/imagen1.jpg',
     },
     {
       title: ['REFRESCAMIENTO'],
-      body: ['Mediante Losa Radiante y/o Fan Coils','Ambientes unificados o diferenciados','Temperatura mínima recomendada en Frio aereo: 24°C'],
-      img:'/garden-800.jpg'
+      body: [
+        'Mediante Losa Radiante y/o Fan Coils',
+        'Ambientes unificados o diferenciados',
+        'Temperatura mínima recomendada en Frio aereo: 24°C',
+      ],
+      img: '/paisajes/imagen2.jpg',
     },
     {
       title: ['AGUA CALIENTE SANITARIA'],
-      body: ['Mediante Tanque Acumulador Termico', 'Agua caliente a 40°C, extensible hasta 70°C', 'Sistema secundario ante emergencias'],
-      img:'/garden-800.jpg'
+      body: [
+        'Mediante Tanque Acumulador Termico',
+        'Agua caliente a 40°C, extensible hasta 70°C',
+        'Sistema secundario ante emergencias',
+      ],
+      img: '/paisajes/imagen3.jpg',
     },
   ];
 
-   slide = signal<Slide>(this.SLIDES[0]);
-
   nextSlide() {
-    this.slideIndex = (this.slideIndex + 1) % this.SLIDES.length;
-    this.direction = 'toRight'
-    this.slide.set(this.SLIDES[this.slideIndex]);
+    //Primero suma el index
+    this.index = (this.index + 1) % this.SLIDES.length;
+
+    
+    if (this.auxActive) {
+      this.indexA = this.index;
+    } else {
+      this.indexB = this.index;
+    }
+
+    this.direction = 'toRight';
     this.auxActive = !this.auxActive;
+
+    // Actualiza el indexA para que coincida con el index actual
+    if (this.auxActive) {
+      this.indexA = this.index;
+    } else {
+      this.indexB = this.index;
+    }
   }
 
   prevSlide() {
-    this.slideIndex =
-      (this.slideIndex - 1 + this.SLIDES.length) % this.SLIDES.length;
-      this.direction = 'toLeft'
-      this.slide.set(this.SLIDES[this.slideIndex]);
-      this.auxActive = !this.auxActive;
-  }
-
-  isBarFull(event:boolean){
-    this.nextSlide()
     
+    
+    this.index = (this.index - 1 + this.SLIDES.length) % this.SLIDES.length;
+    
+        if (this.auxActive) {
+      this.indexA = this.index;
+    } else {
+      this.indexB = this.index;
+    }
+    this.direction = 'toLeft';
+    this.auxActive = !this.auxActive;
+
+        if (this.auxActive) {
+      this.indexA = this.index;
+    } else {
+      this.indexB = this.index;
+    }
   }
 
+  isBarFull(event: boolean) {
+    this.nextSlide();
+  }
 
+  onAnimationDone(event: AnimationEvent) {
+    if (event.toState === 'toLeft' || event.toState === 'toRight') {
+      this.direction = 'default';
+    }
+  }
 }
--- src/app/pages/servicio/servicio.component.ts
+++ src/app/pages/servicio/servicio.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ServicioItemsComponent } from '../../components/servicio-items/servicio-items.component';
 import { fade, triggerAnimations } from '../../animations';
 import { SliderComponent } from '../../components/slider/slider.component';
 
@@ -8,7 +7,7 @@ import { SliderComponent } from '../../components/slider/slider.component';
     templateUrl: './servicio.component.html',
     styleUrl: './servicio.component.css',
     standalone:true,
-    imports:[ServicioItemsComponent, SliderComponent],
+    imports:[SliderComponent],
     animations: [triggerAnimations,fade]
 })
 export class ServicioComponent {
